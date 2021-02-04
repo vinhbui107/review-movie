203d192b4aeaf1b24a283a1470b80f14c9b26edf
vinhbui107 <vinhbui.tg@gmail.com>
2021-02-05 03:03:46 +0700

    create genre, actor, movie, rate models

--- api/apps/reviews/models.py
+++ api/apps/reviews/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from apps.accounts.models import User
+from apps.movies.models import Movie
+
+
+class Review(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="reviews", null=False, blank=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reviews", null=False, blank=False)
+    content = models.TextField()
+
+
+class Reply(models.Model):
+    review = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="replies", null=False, blank=False)
+    user = models.ForeignKey(Review, on_delete=models.CASCADE, related_name="replies", null=False, blank=False)
+    content = models.TextField()
