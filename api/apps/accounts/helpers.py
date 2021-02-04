a960a53f5d392e21e8da6e2eaf10d34295fbb7ab
vinhbui107 <vinhbui.tg@gmail.com>
2021-02-05 03:02:35 +0700

    create account model

---
+++ api/apps/accounts/helpers.py
@@ -0,0 +1,18 @@
+import uuid
+from os.path import splitext
+
+
+def upload_to_user_avatar_directory(user, filename):
+    return _upload_to_user_directory(user, filename=filename)
+
+
+def _upload_to_user_directory(user, filename):
+    extension = splitext(filename)[1].lower()
+    new_filename = str(uuid.uuid4()) + extension
+
+    path = "users/%(user_uuid)s/" % {"user_uuid": str(user.uuid)}
+
+    return "%(path)s%(new_filename)s" % {
+        "path": path,
+        "new_filename": new_filename,
+    }
