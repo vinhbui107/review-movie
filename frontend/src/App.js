58b48f2b9f3f2ae0be274ad7526617a32ef73373
vinhbui107 <vinhbui.tg@gmail.com>
2021-02-14 22:03:52 +0700

    fix not found React

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
 function App() {
   return (
@@ -9,12 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
