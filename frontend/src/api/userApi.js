0836bf4daf181e6d5efb48103e0cd9643885d98d
vinhbui107 <vinhbui.tg@gmail.com>
2021-02-23 08:20:34 +0700

    create files for api folder

---
+++ frontend/src/api/userApi.js
@@ -0,0 +1,14 @@
+import axiosClient from "./axiosClient";
+
+const userApi = {
+    login: () => {
+        // const url = "/products";
+        // return axiosClient.get(url, { params });
+    },
+
+    register: () => {},
+
+    confirmRegister: () => {},
+};
+
+export default userApi;
