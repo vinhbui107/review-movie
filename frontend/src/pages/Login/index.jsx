ec81e657c520aa494c885e8d26e53ac7caa4b39b
vinhbui107 <vinhbui.tg@gmail.com>
2021-02-25 10:18:01 +0700

    update format code for pages

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 function Login() {
-    return (
-        <div>
-            <h2>Login</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2>Login</h2>
+    </div>
+  );
 }
 
 export default Login;
--- frontend/src/pages/Register/index.jsx
+++ frontend/src/pages/Register/index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 function Register() {
-    return (
-        <div>
-            <h2>Register</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2>Register</h2>
+    </div>
+  );
 }
 
 export default Register;
