7175c37495e4710a8d52447b2544f325e669836f
QuachTrienHuy1107 <quachhuy1107@gmail.com>
2021-02-27 23:33:25 +0700

    update scroll for header

--- frontend/src/components/Common/Header/index.jsx
+++ frontend/src/components/Common/Header/index.jsx
@@ -1,20 +1,44 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import PropTypes from "prop-types";
 /* import { NavbarStyled } from "../../../baseUI/navbar"; */
 import { Link, NavLink } from "react-router-dom";
 import "./style.scss";
-import { Navbar, Nav, Form, FormControl, Container, NavDropdown } from "react-bootstrap";
-import { Button } from "reactstrap";
+import { Navbar, Nav, Form, FormControl, Container, NavDropdown, Button } from "react-bootstrap";
 import "antd/dist/antd.css";
 import Banner from "../Banner";
+import { MDBCol, MDBInput } from "mdbreact";
 
 Header.propTypes = {};
 
 function Header(props) {
+  const [navBackground, setNavBackground] = useState(false);
+  const [searchForm, setSearchForm] = useState(false);
+  const navRef = useRef();
+  navRef.current = navBackground;
+  useEffect(() => {
+    const handleScroll = () => {
+      const show = window.scrollY > 50;
+      if (navRef.current !== show) {
+        setNavBackground(show);
+      }
+    };
+    document.addEventListener("scroll", handleScroll);
+    return () => {
+      document.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleSearchForm = () => {
+    return searchForm ? "search__form__click" : "";
+  };
+
   return (
     <>
       <Banner />
-      <div className="header">
+      <div
+        className="header"
+        style={{ transition: "1s ease", backgroundColor: navBackground ? "#000" : "transparent" }}
+      >
         <Container>
           <Navbar expand="lg" variant="light">
             <Navbar.Brand href="#home">
@@ -25,10 +49,10 @@ function Header(props) {
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="mr-auto">
                 <NavLink to="/" className="nav-link nav-link-header">
-                  HOMEs
+                  HOME
                 </NavLink>
 
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown title="Categories" id="basic-nav-dropdown">
                   <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                   <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                   <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
@@ -36,16 +60,16 @@ function Header(props) {
                   <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                 </NavDropdown>
               </Nav>
+
+              <Form inline className={` ${handleSearchForm()} form__search flex-nowrap mx-4`}>
+                <i className="fa fa-search mr-2" onClick={() => setSearchForm(!searchForm)} />
+                <input type="text" placeholder="Search" className="form__search__custom pl-0" />
+              </Form>
               <Nav>
                 <NavLink to="/login" className="nav-link nav-link-header mr-3">
                   Login
                 </NavLink>
               </Nav>
-
-              <Form inline>
-                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                <Button variant="outline-danger">Search</Button>
-              </Form>
             </Navbar.Collapse>
           </Navbar>
         </Container>
