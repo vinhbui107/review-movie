321896d2f3558b5a6178b7b6d57fa97c0f728478
vinhbui107 <vinhbui.tg@gmail.com>
2021-03-02 08:23:34 +0700

    create url for auth

--- api/config/urls.py
+++ api/config/urls.py
@@ -1,21 +1,36 @@
-"""config URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls import include
+from django.conf.urls.static import static
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
+auth_patterns = [
+    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("register/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+]
+
+movie_patterns = []
+
+review_patterns = []
+
+recommend_patterns = []
+
+api_patterns = [
+    path("auth/", include(auth_patterns)),
+    path("movies/", include(movie_patterns)),
+    path("reviews/", include(review_patterns)),
+    path("recommend/", include(recommend_patterns)),
+]
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("api/", include(api_patterns)),
+    path("admin/", admin.site.urls),
 ]
