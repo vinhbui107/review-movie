07b644c747c6a39ce1e186ab9449567397b009ab
vinhbui107 <vinhbui.tg@gmail.com>
2021-03-02 08:20:54 +0700

    Update format code

--- api/apps/reviews/models.py
+++ api/apps/reviews/models.py
@@ -6,12 +6,36 @@ from apps.movies.models import Movie
 
 
 class Review(models.Model):
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="reviews", null=False, blank=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reviews", null=False, blank=False)
+    movie = models.ForeignKey(
+        Movie,
+        on_delete=models.CASCADE,
+        related_name="reviews",
+        null=False,
+        blank=False,
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="reviews",
+        null=False,
+        blank=False,
+    )
     content = models.TextField()
 
 
 class Reply(models.Model):
-    review = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="replies", null=False, blank=False)
-    user = models.ForeignKey(Review, on_delete=models.CASCADE, related_name="replies", null=False, blank=False)
+    review = models.ForeignKey(
+        Movie,
+        on_delete=models.CASCADE,
+        related_name="replies",
+        null=False,
+        blank=False,
+    )
+    user = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name="replies",
+        null=False,
+        blank=False,
+    )
     content = models.TextField()
