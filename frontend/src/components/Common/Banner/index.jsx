75e2885f90e8e0ab4da1b24782faa41aa09a3a80
vinhbui107 <vinhbui.tg@gmail.com>
2021-03-03 03:19:57 +0700

    update format code

--- frontend/src/components/Common/Banner/index.jsx
+++ frontend/src/components/Common/Banner/index.jsx
@@ -13,48 +13,110 @@ function Banner(props) {
         setIndex(selectedIndex);
     };
 
-  const stylee = {
-    backgroundImage: `url("https://image.tmdb.org/t/p/w500//8veOfB9RbSzFki0Rq3IQIGsFfhC.jpg")`,
-  };
+    const stylee = {
+        backgroundImage: `url("https://image.tmdb.org/t/p/w500//8veOfB9RbSzFki0Rq3IQIGsFfhC.jpg")`,
+    };
 
-  return (
-    <>
-      <div style={{ position: "relative" }}>
-        <Tabs id="controlled-tab-example" activeKey={key} onSelect={(k) => setKey(k)} className="tab">
-          <Tab
-            eventKey="home"
-            title="  "
-            className="tab__item tab__item-1"
-            style={{ backgroundImage: `url("./img/hero-1.jpg")` }}
-          >
-            <div className="tab__caption text-left container">
-              <div className="tab__caption__info ">
-                <h3>Scoob!</h3>
-                <div className="tab__caption__category">
-                  <ul className="tab__caption__category__list">
-                    <li>IMDB 5.3</li>
-                    <li>
-                      <a href="/">Animation</a>
-                    </li>
-                  </ul>
-                </div>
-                <p>
-                  Scooby-Doo và the Mystery Inc. hợp sức giải cứu thế giới khỏi tên ác nhân Dick Dastardly. Cùng đón xem
-                  bộ phim những cuộc phưu lưu của chú chó scooby doo (2…
-                </p>
-                <div className="play d-flex align-items-center">
-                  <button className="text-white">
-                    <i className="fa fa-play"></i>
-                    Play trailer
-                  </button>
-                  <button className="text-white">
-                    <i className="fa fa-play"></i>
-                    Yeu thich
-                  </button>
-                </div>
-              </div>
+    return (
+        <>
+            <div style={{ position: "relative" }}>
+                <Tabs id="controlled-tab-example" activeKey={key} onSelect={(k) => setKey(k)} className="tab">
+                    <Tab
+                        eventKey="home"
+                        title="  "
+                        className="tab__item tab__item-1"
+                        style={{ backgroundImage: `url("./img/hero-1.jpg")` }}
+                    >
+                        <div className="tab__caption text-left container">
+                            <div className="tab__caption__info ">
+                                <h3>Scoob!</h3>
+                                <div className="tab__caption__category">
+                                    <ul className="tab__caption__category__list">
+                                        <li>IMDB 5.3</li>
+                                        <li>
+                                            <a href="/">Animation</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <p>
+                                    Scooby-Doo và the Mystery Inc. hợp sức giải cứu thế giới khỏi tên ác nhân Dick
+                                    Dastardly. Cùng đón xem bộ phim những cuộc phưu lưu của chú chó scooby doo (2…
+                                </p>
+                                <div className="play d-flex align-items-center">
+                                    <button className="text-white">
+                                        <i className="fa fa-play"></i>
+                                        Play trailer
+                                    </button>
+                                    <button className="text-white">
+                                        <i className="fa fa-play"></i>
+                                        Yeu thich
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </Tab>
+                    <Tab eventKey="profile" title="Phim 2" className="tab__item tab__item-2">
+                        <div className="tab__caption text-left container">
+                            <div className="tab__item__info w-50">
+                                <h3>Scoob!</h3>
+                                <div className="tab__caption__category">
+                                    <ul className="tab__caption__category__list">
+                                        <li>IMDB 5.3</li>
+                                        <li>
+                                            <a href="/">Animation</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <h1>
+                                    Scooby-Doo và the Mystery Inc. hợp sức giải cứu thế giới khỏi tên ác nhân Dick
+                                    Dastardly. Cùng đón xem bộ phim những cuộc phưu lưu của chú chó scooby doo (2…
+                                </h1>
+                                <div className="play d-flex align-items-center">
+                                    <button className="text-white">
+                                        <i className="fa fa-play"></i>
+                                        Play trailer
+                                    </button>
+                                    <button className="text-white">
+                                        <i className="fa fa-play"></i>
+                                        Like
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </Tab>
+                    <Tab eventKey="contact" title="Phim 3" className="tab__item tab__item-3">
+                        <div className="tab__caption text-left container">
+                            <div className="tab__item__info w-50">
+                                <h3>Scoob!</h3>
+                                <div className="tab__caption__category">
+                                    <ul className="tab__caption__category__list">
+                                        <li>IMDB 5.3</li>
+                                        <li>
+                                            <a href="/">Animation</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <p>
+                                    Scooby-Doo và the Mystery Inc. hợp sức giải cứu thế giới khỏi tên ác nhân Dick
+                                    Dastardly. Cùng đón xem bộ phim những cuộc phưu lưu của chú chó scooby doo (2…
+                                </p>
+                                <div className="play d-flex align-items-center">
+                                    <button className="text-white">
+                                        <i className="fa fa-play"></i>
+                                        Play trailer
+                                    </button>
+                                    <button className="text-white">
+                                        <i className="fa fa-play"></i>
+                                        Yeu thich
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </Tab>
+                </Tabs>
             </div>
         </>
     );
+}
 
 export default Banner;
