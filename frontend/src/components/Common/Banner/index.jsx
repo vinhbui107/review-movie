f22044ed150928441298166e026046bd2a22ec27
vinhbui107 <vinhbui.tg@gmail.com>
2021-03-02 08:19:31 +0700

    format code to spaces 4

--- frontend/src/components/Common/Banner/index.jsx
+++ frontend/src/components/Common/Banner/index.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import PropTypes from "prop-types";
 import { Carousel, Tabs, Tab } from "react-bootstrap";
 import "./style.scss";
 import { Container, Row, Col } from "react-bootstrap";
@@ -7,108 +6,108 @@ import { Container, Row, Col } from "react-bootstrap";
 Banner.propTypes = {};
 
 function Banner(props) {
-  const [index, setIndex] = useState(0);
-  const [key, setKey] = useState("home");
+    const [index, setIndex] = useState(0);
+    const [key, setKey] = useState("home");
 
-  const handleSelect = (selectedIndex, e) => {
-    setIndex(selectedIndex);
-  };
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex);
+    };
 
-  return (
-    <>
-      <div style={{ position: "relative" }}>
-        <Tabs id="controlled-tab-example" activeKey={key} onSelect={(k) => setKey(k)} className="tab">
-          <Tab eventKey="home" title="Phim 1" className="tab__item tab__item-1">
-            <div className="tab__caption text-left container">
-              <div className="tab__caption__info w-50">
-                <h3>Scoob!</h3>
-                <div className="tab__caption__category">
-                  <ul className="tab__caption__category__list">
-                    <li>IMDB 5.3</li>
-                    <li>
-                      <a href="/">Animation</a>
-                    </li>
-                  </ul>
-                </div>
-                <p>
-                  Scooby-Doo và the Mystery Inc. hợp sức giải cứu thế giới khỏi tên ác nhân Dick Dastardly. Cùng đón xem
-                  bộ phim những cuộc phưu lưu của chú chó scooby doo (2…
-                </p>
-                <div className="play d-flex align-items-center">
-                  <button className="text-white">
-                    <i className="fa fa-play"></i>
-                    Play trailer
-                  </button>
-                  <button className="text-white">
-                    <i className="fa fa-play"></i>
-                    Yeu thich
-                  </button>
-                </div>
-              </div>
+    return (
+        <>
+            <div style={{ position: "relative" }}>
+                <Tabs id="controlled-tab-example" activeKey={key} onSelect={(k) => setKey(k)} className="tab">
+                    <Tab eventKey="home" title="Phim 1" className="tab__item tab__item-1">
+                        <div className="tab__caption text-left container">
+                            <div className="tab__caption__info w-50">
+                                <h3>Scoob!</h3>
+                                <div className="tab__caption__category">
+                                    <ul className="tab__caption__category__list">
+                                        <li>IMDB 5.3</li>
+                                        <li>
+                                            <a href="/">Animation</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <p>
+                                    Scooby-Doo và the Mystery Inc. hợp sức giải cứu thế giới khỏi tên ác nhân Dick
+                                    Dastardly. Cùng đón xem bộ phim những cuộc phưu lưu của chú chó scooby doo (2…
+                                </p>
+                                <div className="play d-flex align-items-center">
+                                    <button className="text-white">
+                                        <i className="fa fa-play"></i>
+                                        Play trailer
+                                    </button>
+                                    <button className="text-white">
+                                        <i className="fa fa-play"></i>
+                                        Yeu thich
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </Tab>
+                    <Tab eventKey="profile" title="Phim 2" className="tab__item tab__item-2">
+                        <div className="tab__caption text-left container">
+                            <div className="tab__item__info w-50">
+                                <h3>Scoob!</h3>
+                                <div className="tab__caption__category">
+                                    <ul className="tab__caption__category__list">
+                                        <li>IMDB 5.3</li>
+                                        <li>
+                                            <a href="/">Animation</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <h1>
+                                    Scooby-Doo và the Mystery Inc. hợp sức giải cứu thế giới khỏi tên ác nhân Dick
+                                    Dastardly. Cùng đón xem bộ phim những cuộc phưu lưu của chú chó scooby doo (2…
+                                </h1>
+                                <div className="play d-flex align-items-center">
+                                    <button className="text-white">
+                                        <i className="fa fa-play"></i>
+                                        Play trailer
+                                    </button>
+                                    <button className="text-white">
+                                        <i className="fa fa-play"></i>
+                                        Like
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </Tab>
+                    <Tab eventKey="contact" title="Phim 3" className="tab__item tab__item-3">
+                        <div className="tab__caption text-left container">
+                            <div className="tab__item__info w-50">
+                                <h3>Scoob!</h3>
+                                <div className="tab__caption__category">
+                                    <ul className="tab__caption__category__list">
+                                        <li>IMDB 5.3</li>
+                                        <li>
+                                            <a href="/">Animation</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <p>
+                                    Scooby-Doo và the Mystery Inc. hợp sức giải cứu thế giới khỏi tên ác nhân Dick
+                                    Dastardly. Cùng đón xem bộ phim những cuộc phưu lưu của chú chó scooby doo (2…
+                                </p>
+                                <div className="play d-flex align-items-center">
+                                    <button className="text-white">
+                                        <i className="fa fa-play"></i>
+                                        Play trailer
+                                    </button>
+                                    <button className="text-white">
+                                        <i className="fa fa-play"></i>
+                                        Yeu thich
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </Tab>
+                </Tabs>
             </div>
-          </Tab>
-          <Tab eventKey="profile" title="Phim 2" className="tab__item tab__item-2">
-            <div className="tab__caption text-left container">
-              <div className="tab__item__info w-50">
-                <h3>Scoob!</h3>
-                <div className="tab__caption__category">
-                  <ul className="tab__caption__category__list">
-                    <li>IMDB 5.3</li>
-                    <li>
-                      <a href="/">Animation</a>
-                    </li>
-                  </ul>
-                </div>
-                <h1>
-                  Scooby-Doo và the Mystery Inc. hợp sức giải cứu thế giới khỏi tên ác nhân Dick Dastardly. Cùng đón xem
-                  bộ phim những cuộc phưu lưu của chú chó scooby doo (2…
-                </h1>
-                <div className="play d-flex align-items-center">
-                  <button className="text-white">
-                    <i className="fa fa-play"></i>
-                    Play trailer
-                  </button>
-                  <button className="text-white">
-                    <i className="fa fa-play"></i>
-                    Like
-                  </button>
-                </div>
-              </div>
-            </div>
-          </Tab>
-          <Tab eventKey="contact" title="Phim 3" className="tab__item tab__item-3">
-            <div className="tab__caption text-left container">
-              <div className="tab__item__info w-50">
-                <h3>Scoob!</h3>
-                <div className="tab__caption__category">
-                  <ul className="tab__caption__category__list">
-                    <li>IMDB 5.3</li>
-                    <li>
-                      <a href="/">Animation</a>
-                    </li>
-                  </ul>
-                </div>
-                <p>
-                  Scooby-Doo và the Mystery Inc. hợp sức giải cứu thế giới khỏi tên ác nhân Dick Dastardly. Cùng đón xem
-                  bộ phim những cuộc phưu lưu của chú chó scooby doo (2…
-                </p>
-                <div className="play d-flex align-items-center">
-                  <button className="text-white">
-                    <i className="fa fa-play"></i>
-                    Play trailer
-                  </button>
-                  <button className="text-white">
-                    <i className="fa fa-play"></i>
-                    Yeu thich
-                  </button>
-                </div>
-              </div>
-            </div>
-          </Tab>
-        </Tabs>
-      </div>
-    </>
-  );
+        </>
+    );
 }
 
 export default Banner;
--- frontend/src/components/Common/Header/index.jsx
+++ frontend/src/components/Common/Header/index.jsx
@@ -1,81 +1,82 @@
 import React, { useEffect, useRef, useState } from "react";
-import PropTypes from "prop-types";
-/* import { NavbarStyled } from "../../../baseUI/navbar"; */
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./style.scss";
-import { Navbar, Nav, Form, FormControl, Container, NavDropdown, Button } from "react-bootstrap";
+import { Navbar, Nav, Form, NavDropdown, Container } from "react-bootstrap";
 import "antd/dist/antd.css";
 import Banner from "../Banner";
-import { MDBCol, MDBInput } from "mdbreact";
 
 Header.propTypes = {};
 
 function Header(props) {
-  const [navBackground, setNavBackground] = useState(false);
-  const [searchForm, setSearchForm] = useState(false);
-  const navRef = useRef();
-  navRef.current = navBackground;
-  useEffect(() => {
-    const handleScroll = () => {
-      const show = window.scrollY > 50;
-      if (navRef.current !== show) {
-        setNavBackground(show);
-      }
+    const [navBackground, setNavBackground] = useState(false);
+    const [searchForm, setSearchForm] = useState(false);
+    const navRef = useRef();
+    navRef.current = navBackground;
+    useEffect(() => {
+        const handleScroll = () => {
+            const show = window.scrollY > 50;
+            if (navRef.current !== show) {
+                setNavBackground(show);
+            }
+        };
+        document.addEventListener("scroll", handleScroll);
+        return () => {
+            document.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    const handleSearchForm = () => {
+        return searchForm ? "search__form__click" : "";
     };
-    document.addEventListener("scroll", handleScroll);
-    return () => {
-      document.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
 
-  const handleSearchForm = () => {
-    return searchForm ? "search__form__click" : "";
-  };
+    return (
+        <>
+            <Banner />
+            <div
+                className="header"
+                style={{ transition: "1s ease", backgroundColor: navBackground ? "#000" : "transparent" }}
+            >
+                <Container>
+                    <Navbar expand="lg" variant="light">
+                        <Navbar.Brand href="#home">
+                            <img
+                                src="./img/logo.svg"
+                                style={{ position: "relative", bottom: "5px", right: "15px" }}
+                                alt=""
+                            />
+                        </Navbar.Brand>
 
-  return (
-    <>
-      <Banner />
-      <div
-        className="header"
-        style={{ transition: "1s ease", backgroundColor: navBackground ? "#000" : "transparent" }}
-      >
-        <Container>
-          <Navbar expand="lg" variant="light">
-            <Navbar.Brand href="#home">
-              <img src="./img/logo.svg" style={{ position: "relative", bottom: "5px", right: "15px" }} alt="" />
-            </Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="mr-auto">
+                                <NavLink to="/" className="nav-link nav-link-header">
+                                    HOME
+                                </NavLink>
 
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="mr-auto">
-                <NavLink to="/" className="nav-link nav-link-header">
-                  HOME
-                </NavLink>
+                                <NavDropdown title="Categories" id="basic-nav-dropdown">
+                                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                    <NavDropdown.Divider />
+                                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                                </NavDropdown>
+                            </Nav>
 
-                <NavDropdown title="Categories" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                </NavDropdown>
-              </Nav>
-
-              <Form inline className={` ${handleSearchForm()} form__search flex-nowrap mx-4`}>
-                <i className="fa fa-search mr-2" onClick={() => setSearchForm(!searchForm)} />
-                <input type="text" placeholder="Search" className="form__search__custom pl-0" />
-              </Form>
-              <Nav>
-                <NavLink to="/login" className="nav-link nav-link-header mr-3">
-                  Login
-                </NavLink>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-        </Container>
-      </div>
-    </>
-  );
+                            <Form inline className={` ${handleSearchForm()} form__search flex-nowrap mx-4`}>
+                                <i className="fa fa-search mr-2" onClick={() => setSearchForm(!searchForm)} />
+                                <input type="text" placeholder="Search" className="form__search__custom pl-0" />
+                            </Form>
+                            <Nav>
+                                <NavLink to="/login" className="nav-link nav-link-header mr-3">
+                                    Login
+                                </NavLink>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
+                </Container>
+            </div>
+        </>
+    );
 }
 
 export default Header;
