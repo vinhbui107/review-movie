b2a603f8ac559b0fdaa2fbdedba91e47ff41be07
QuachTrienHuy1107 <quachhuy1107@gmail.com>
2021-03-04 23:44:27 +0700

    Fix search form navbar

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 import "./style.scss";
 import { Navbar, Nav, Form, Container, NavDropdown } from "react-bootstrap";
 import "antd/dist/antd.css";
-import Banner from "../Banner";
+import { handleSearchForm, userIcon } from "../../utils/constants";
 
 Header.propTypes = {};
 
@@ -25,20 +25,19 @@ function Header(props) {
         };
     }, []);
 
-    const handleSearchForm = () => {
-        return searchForm ? "search__form__click" : "";
-    };
-
-    const user = () => {
-        return <i class="fa fa-user mx-3"></i>;
-    };
-
     return (
         <>
             <div
                 className="header"
                 style={{ transition: "1s ease", backgroundColor: navBackground ? "#232323" : "transparent" }}
             >
+                <div
+                    className={`${searchForm ? "header__overlay" : ""}`}
+                    onClick={() => {
+                        setSearchForm();
+                        console.log(searchForm);
+                    }}
+                ></div>
                 <Container fluid>
                     <Navbar expand="lg" variant="light">
                         <Navbar.Brand href="#home">
@@ -65,7 +64,7 @@ function Header(props) {
                                 </NavLink>
                             </Nav>
 
-                            <Form inline className={` ${handleSearchForm()} form__search flex-nowrap mx-4`}>
+                            <Form inline className={` ${handleSearchForm(searchForm)} form__search flex-nowrap mx-4`}>
                                 <i className="fa fa-search mr-2" onClick={() => setSearchForm(!searchForm)} />
                                 <input type="text" placeholder="Search" className="form__search__custom pl-0" />
                             </Form>
@@ -74,7 +73,7 @@ function Header(props) {
                                     Login
                                 </NavLink> */}
 
-                                <NavDropdown title={user()} id="basic-nav-dropdown" className="mr-5 d-flex">
+                                <NavDropdown title={userIcon()} id="basic-nav-dropdown" className="mr-5 d-flex">
                                     <NavDropdown.Item href="#action/3.1">Info</NavDropdown.Item>
                                     <NavDropdown.Item href="#action/3.2">Log out</NavDropdown.Item>
                                     <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
