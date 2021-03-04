2d97b7dbfec1a35ea28463bf6c9a125ca6fc5334
QuachTrienHuy1107 <quachhuy1107@gmail.com>
2021-03-04 23:48:25 +0700

    change width container

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,23 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import OwlCarousel from "react-owl-carousel";
 
 import { Container, Row, Col } from "react-bootstrap";
 import "./style.scss";
 
 import MovieList from "../../components/MovieList";
-import Banner from "../../components/Banner";
+import Carousel from "../../components/Carousel";
 
 function Home() {
     return (
         <div>
-            <Banner />
-            <Container>
-                <h1 className="title">Reccommend</h1>
-                <MovieList />
-                <h1 className="title">
-                    <span className="title__logo">1</span> Top Rating
-                </h1>
-                <MovieList />
+            <Carousel />
+            <Container fluid style={{ width: "85%" }}>
+                <MovieList title={"Reccommend"} />
+                <MovieList title={"Top Rating"} logo={"1"} items={10} />
             </Container>
         </div>
     );
