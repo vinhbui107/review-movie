caaa558b514d5cfd4943e9155500509c232afeed
QuachTrienHuy1107 <quachhuy1107@gmail.com>
2021-03-11 01:13:27 +0700

    handle user login

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,8 @@
-import Avatar from "antd/lib/avatar/avatar";
+import { Comment, Avatar, Form, Button, List, Input } from "antd";
+
+import { NavDropdown } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import { isLogin } from "../services/user";
 
 export const layout = {
     labelCol: {
@@ -44,6 +48,22 @@ export const residences = [
     },
 ];
 
+//Header
+export const handleLogin = () => {
+    return isLogin() ? (
+        <>
+            <NavDropdown title={userIcon()} id="basic-nav-dropdown" className="mr-5 userIcon">
+                <NavDropdown.Item href="#action/3.1">Info</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Log out</NavDropdown.Item>
+            </NavDropdown>
+        </>
+    ) : (
+        <NavLink to="/login" className="nav-link nav-link-header mr-3">
+            Login
+        </NavLink>
+    );
+};
+
 //HomePage
 export const handleSearchForm = (searchForm) => {
     return searchForm ? "search__form__click" : "";
@@ -54,3 +74,25 @@ export const userIcon = (avatar) => {
 };
 
 //MovieDetail
+const { TextArea } = Input;
+export const CommentsList = ({ comments }) => (
+    <List
+        dataSource={comments}
+        header={`${comments.length} ${comments.length > 1 ? "replies" : "reply"}`}
+        itemLayout="horizontal"
+        renderItem={(props) => <Comment {...props} />}
+    />
+);
+
+export const Editor = ({ onChange, onSubmit, submitting, value }) => (
+    <>
+        <Form.Item>
+            <TextArea rows={4} onChange={onChange} value={value} />
+        </Form.Item>
+        <Form.Item>
+            <Button htmlType="submit" loading={submitting} onClick={onSubmit} type="primary">
+                Add Comment
+            </Button>
+        </Form.Item>
+    </>
+);
