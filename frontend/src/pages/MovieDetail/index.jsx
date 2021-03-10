3fd32bc6a19e36be31db1290c1cfbe9192dc47ce
QuachTrienHuy1107 <quachhuy1107@gmail.com>
2021-03-11 01:18:07 +0700

    getMovieById api

--- frontend/src/pages/MovieDetail/index.jsx
+++ frontend/src/pages/MovieDetail/index.jsx
@@ -5,115 +5,42 @@ import MovieList from "../../components/MovieList";
 import { Comment, Tooltip, List, Avatar, Form, Button, Input } from "antd";
 import moment from "moment";
 import "./style.scss";
+import { useDispatch, useSelector } from "react-redux";
+import productApi from "../../services/movie";
+import Comments from "../../components/Comment";
+import CommentList from "../../components/CommentList";
 
 const { TextArea } = Input;
 
-const CommentList = ({ comments }) => (
+/* const CommentList = ({ comments }) => (
     <List
         dataSource={comments}
         header={`${comments.length} ${comments.length > 1 ? "replies" : "reply"}`}
         itemLayout="horizontal"
         renderItem={(props) => <Comment {...props} />}
     />
-);
+); */
 
-const Editor = ({ onChange, onSubmit, submitting, value }) => (
-    <>
-        <Form.Item>
-            <TextArea rows={4} onChange={onChange} value={value} />
-        </Form.Item>
-        <Form.Item>
-            <Button htmlType="submit" loading={submitting} onClick={onSubmit} type="primary">
-                Add Comment
-            </Button>
-        </Form.Item>
-    </>
-);
+function MovieDetail(props) {
+    const [movieDetail, setMovieDetail] = useState({});
+    // const stateMovieDetail = useSelector((state) => state.movieReducer.movieDetail);
 
-const data = [
-    {
-        actions: [<span key="comment-list-reply-to-0">Reply to</span>],
-        author: "Han Solo",
-        avatar: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-        content: (
-            <p>
-                We supply a series of design principles, practical patterns and high quality design resources (Sketch
-                and Axure), to help people create their product prototypes beautifully and efficiently.
-            </p>
-        ),
-        datetime: (
-            <Tooltip title={moment().subtract(1, "days").format("YYYY-MM-DD HH:mm:ss")}>
-                <span>{moment().subtract(1, "days").fromNow()}</span>
-            </Tooltip>
-        ),
-    },
-    {
-        actions: [<span key="comment-list-reply-to-0">Reply to</span>],
-        author: "Han Solo",
-        avatar: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-        content: (
-            <p>
-                We supply a series of design principles, practical patterns and high quality design resources (Sketch
-                and Axure), to help people create their product prototypes beautifully and efficiently.
-            </p>
-        ),
-        datetime: (
-            <Tooltip title={moment().subtract(2, "days").format("YYYY-MM-DD HH:mm:ss")}>
-                <span>{moment().subtract(2, "days").fromNow()}</span>
-            </Tooltip>
-        ),
-    },
-];
+    useEffect(() => {
+        const getMovieById = async () => {
+            const response = await productApi.getMovieById(props.match.params.id);
 
-function MovieDetail() {
-    const [comments, setComments] = useState([]);
-    const [submitting, setSubmitting] = useState(false);
-    const [value, setValue] = useState("");
-
-    const handleChange = (e) => {
-        setValue(e.target.value);
-    };
-
-    const handleSubmit = () => {
-        data.push({
-            actions: [<span key="comment-list-reply-to-0">Reply to</span>],
-            author: "Han Solo",
-            avatar: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-            content: <p>{value}</p>,
-            datetime: (
-                <Tooltip title={moment().subtract(1, "days").format("YYYY-MM-DD HH:mm:ss")}>
-                    <span>{moment().subtract(1, "days").fromNow()}</span>
-                </Tooltip>
-            ),
-        });
-
-        if (!value) {
-            return;
-        }
-
-        setSubmitting({ submitting: true });
-
-        setTimeout(() => {
-            setSubmitting();
-            setValue("");
-            setComments([
-                ...comments,
-                {
-                    author: "Han Solo",
-                    avatar: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-                    content: <p>{value}</p>,
-                    datetime: moment().fromNow(),
-                },
-            ]);
-        }, 1000);
-    };
+            console.log("responseMovieDetail", response);
+            setMovieDetail(response);
+        };
+        getMovieById();
+    }, []);
 
     return (
         <>
+            {console.log("Match:", props.match.params.id)}
             <div style={{ backgroundImage: `url("./img/hero-2.jpg")`, height: "700px" }}>
-                <div></div>
                 <p></p>
-                <MovieInfo text={"MovieDetail"} />
+                <MovieInfo text={movieDetail.Director} title={movieDetail.Title} desc={movieDetail.Description} />
             </div>
             <Container>
                 <h2>Recommend</h2>
@@ -122,23 +49,7 @@ function MovieDetail() {
                 <div className="comment">
                     <h1>Comment</h1>
 
-                    {comments.length > 0 && <CommentList comments={comments} />}
-                    <Comment
-                        avatar={
-                            <Avatar
-                                src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-                                alt="Han Solo"
-                            />
-                        }
-                        content={
-                            <Editor
-                                onChange={handleChange}
-                                onSubmit={handleSubmit}
-                                submitting={submitting}
-                                value={value}
-                            />
-                        }
-                    />
+                    <CommentList />
                 </div>
             </Container>
         </>
