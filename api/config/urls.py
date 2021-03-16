c21ce06205041e0c0e7620e23a538ca197089f37
vinhbui107 <vinhbui.tg@gmail.com>
2021-03-16 07:58:12 +0700

    add token and refresh token url

--- api/config/urls.py
+++ api/config/urls.py
@@ -13,12 +13,13 @@ from apps.accounts.views import Register
 
 
 auth_patterns = [
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("register/", Register.as_view(), name="register-user"),
 ]
 
-movie_patterns = []
+movie_patterns = [
+    # path("<str:slug>", Register.as_view(), name="movie-genre"),
+    # path("<str:slug>", Register.as_view(), name="movie-detail"),
+]
 
 review_patterns = []
 
@@ -35,4 +36,6 @@ api_patterns = [
 urlpatterns = [
     path("api/", include(api_patterns)),
     path("admin/", admin.site.urls),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
