b1c01e2474bf2791ffee46285dc0d8e998581d96
vinhbui107 <vinhbui.tg@gmail.com>
2021-03-17 20:55:24 +0700

    remove code in comment component

--- frontend/src/components/Comment/index.jsx
+++ frontend/src/components/Comment/index.jsx
@@ -1,51 +1,51 @@
-import React, { useState } from "react";
-import { CommentsList, Editor } from "../../utils/constants";
-import { Comment, Avatar, Form, Button, List, Input } from "antd";
-import moment from "moment";
+// import React, { useState } from "react";
+// import { CommentsList, Editor } from "../../utils/constants";
+// import { Comment, Avatar, Form, Button, List, Input } from "antd";
+// import moment from "moment";
 
-function Comments(props) {
-    const [comments, setComments] = useState([]);
-    const [submitting, setSubmitting] = useState(false);
-    const [value, setValue] = useState("");
+// function Comments(props) {
+//     const [comments, setComments] = useState([]);
+//     const [submitting, setSubmitting] = useState(false);
+//     const [value, setValue] = useState("");
 
-    const handleChange = (e) => {
-        setValue(e.target.value);
-    };
+//     const handleChange = (e) => {
+//         setValue(e.target.value);
+//     };
 
-    const handleSubmit = () => {
-        if (!value) {
-            return;
-        }
+//     const handleSubmit = () => {
+//         if (!value) {
+//             return;
+//         }
 
-        setSubmitting({ submitting: true });
+//         setSubmitting({ submitting: true });
 
-        setTimeout(() => {
-            setSubmitting();
-            setValue("");
-            setComments([
-                ...comments,
-                {
-                    author: "Han Solo",
-                    avatar: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-                    content: <p>{value}</p>,
-                    datetime: moment().fromNow(),
-                },
-            ]);
-        }, 1000);
-    };
-    return (
-        <div>
-            {comments.length > 0 && <CommentsList comments={comments} />}
-            <Comment
-                avatar={
-                    <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" alt="Han Solo" />
-                }
-                content={
-                    <Editor onChange={handleChange} onSubmit={handleSubmit} submitting={submitting} value={value} />
-                }
-            />
-        </div>
-    );
-}
+//         setTimeout(() => {
+//             setSubmitting();
+//             setValue("");
+//             setComments([
+//                 ...comments,
+//                 {
+//                     author: "Han Solo",
+//                     avatar: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
+//                     content: <p>{value}</p>,
+//                     datetime: moment().fromNow(),
+//                 },
+//             ]);
+//         }, 1000);
+//     };
+//     return (
+//         <div>
+//             {comments.length > 0 && <CommentsList comments={comments} />}
+//             <Comment
+//                 avatar={
+//                     <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" alt="Han Solo" />
+//                 }
+//                 content={
+//                     <Editor onChange={handleChange} onSubmit={handleSubmit} submitting={submitting} value={value} />
+//                 }
+//             />
+//         </div>
+//     );
+// }
 
-export default Comments;
+// export default Comments;
