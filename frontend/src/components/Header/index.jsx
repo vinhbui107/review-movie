a7661a110b518b0842d7355822a651c8c855c269
vinhbui107 <vinhbui.tg@gmail.com>
2021-04-07 02:12:12 +0700

    update header render

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,42 +1,62 @@
-import React, { useEffect, useRef, useState } from "react";
-import { Container, Form, Nav, Navbar, NavDropdown, Row } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
-
+import React, { useState } from "react";
+import { Container, Nav, Navbar, NavDropdown, Row } from "react-bootstrap";
+import * as Helpers from "../../utils/helpers.js";
+import logo from "../../assets/img/logo.svg";
 import "./style.scss";
+import userApi from "../../services/user.js";
 
-Header.propTypes = {};
+function Header() {
+    const [username, setUsername] = useState(() => {
+        const initUsername = Helpers.getLocalStorage("name");
+        return initUsername;
+    });
 
-function Header(props) {
-    const [searchForm, setSearchForm] = useState(false);
+    const handleLogout = async () => {
+        await userApi.logout();
+        Helpers.removeLocalStorage("access_token");
+        Helpers.removeLocalStorage("refresh_token");
+        setUsername("");
+    };
 
     return (
-        <>
-            <div className="header">
-                <Container>
-                    <Row>
-                        <Navbar expand="lg">
-                            <Navbar.Brand href="#home">
-                                <img src="https://react.semantic-ui.com/logo.png" style={{ width: "40px" }} alt="" />
-                            </Navbar.Brand>
-                            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                            <Navbar.Collapse id="basic-navbar-nav">
-                                <Nav className="mr-auto">
-                                    <Nav.Link href="#home">Home</Nav.Link>
-                                    <Nav.Link href="#link">Link</Nav.Link>
-                                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                        <NavDropdown.Divider />
-                                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        <div className="header">
+            <Container>
+                <Row>
+                    <Navbar expand="lg">
+                        <Navbar.Brand href="/">
+                            <img src={logo} style={{ width: "154px", height: "50px" }}></img>
+                        </Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="mr-auto">
+                                <NavDropdown title="Movies" id="basic-nav-dropdown">
+                                    <NavDropdown.Item href="#action/3.1">Popular</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.3">Top Rated</NavDropdown.Item>
+                                </NavDropdown>
+                                <Nav.Link href="#link">Recommend</Nav.Link>
+                            </Nav>
+                            {Helpers.isLogin() ? (
+                                <Nav>
+                                    <NavDropdown
+                                        title={username}
+                                        id="basic-nav-dropdown"
+                                        className="dropdown-menu-lg-right"
+                                    >
+                                        <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
+                                        <NavDropdown.Item onClick={handleLogout}>Logout</NavDropdown.Item>
                                     </NavDropdown>
                                 </Nav>
-                            </Navbar.Collapse>
-                        </Navbar>
-                    </Row>
-                </Container>
-            </div>
-        </>
+                            ) : (
+                                <Nav>
+                                    <Nav.Link href="/login">Login</Nav.Link>
+                                    <Nav.Link href="/register">Join TMDB</Nav.Link>
+                                </Nav>
+                            )}
+                        </Navbar.Collapse>
+                    </Navbar>
+                </Row>
+            </Container>
+        </div>
     );
 }
 
