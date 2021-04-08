3b80a99c3eeb1974cae03df76ef052cfaf32576a
vinhbui107 <vinhbui.tg@gmail.com>
2021-04-09 03:24:50 +0700

    update search component

--- frontend/src/components/SearchForm/index.jsx
+++ frontend/src/components/SearchForm/index.jsx
@@ -1,15 +1,61 @@
 import React from "react";
 import { Row } from "react-bootstrap";
+import { ReactSearchAutocomplete } from "react-search-autocomplete";
 import "./style.scss";
 
-Banner.propTypes = {};
+function Banner() {
+    const items = [
+        {
+            id: 0,
+            name: "Cobol",
+        },
+        {
+            id: 1,
+            name: "JavaScript",
+        },
+        {
+            id: 2,
+            name: "Basic",
+        },
+        {
+            id: 3,
+            name: "PHP",
+        },
+        {
+            id: 4,
+            name: "Java",
+        },
+    ];
 
-function Banner(props) {
+    const handleOnSearch = (string, results) => {
+        // onSearch will have as the first callback parameter
+        // the string searched and for the second the results.
+        console.log(string, results);
+    };
+
+    const handleOnSelect = (item) => {
+        // the item selected
+        console.log(item);
+    };
+
+    const handleOnFocus = () => {
+        console.log("Focused");
+    };
     return (
-        <Row>
-            <div className="searchForm">
-                <input type="text" placeholder="Search for anything" />
-                <i className="fa fa-search" />
+        <Row className="search">
+            <div>
+                <div className="search_title">
+                    <h2>Welcome.</h2>
+                    <h3>Thousands of movies, TV shows and people to discover. Explore now.</h3>
+                </div>
+                <div className="search__form">
+                    <ReactSearchAutocomplete
+                        items={items}
+                        onSearch={handleOnSearch}
+                        onSelect={handleOnSelect}
+                        onFocus={handleOnFocus}
+                    />
+                </div>
             </div>
         </Row>
     );
