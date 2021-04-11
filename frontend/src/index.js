e55287e4d0ffece84c99e440b54fc29cbf0449ab
QuachTrienHuy1107 <quachhuy1107@gmail.com>
2021-04-12 00:13:40 +0700

    Add css for antd

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+
+import "antd/dist/antd.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
