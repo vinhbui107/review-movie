a3f2026d8af622dca5dca9c4df0eaef259538fb9
vinhbui107 <vinhbui.tg@gmail.com>
2021-04-21 20:48:05 +0700

    create function search movie

--- api/apps/search/views.py
+++ api/apps/search/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django_elasticsearch_dsl_drf.constants import (
+    LOOKUP_FILTER_TERMS,
+    LOOKUP_FILTER_RANGE,
+    LOOKUP_FILTER_PREFIX,
+    LOOKUP_FILTER_WILDCARD,
+    LOOKUP_QUERY_IN,
+    LOOKUP_QUERY_GT,
+    LOOKUP_QUERY_GTE,
+    LOOKUP_QUERY_LT,
+    LOOKUP_QUERY_LTE,
+    LOOKUP_QUERY_EXCLUDE,
+)
+from django_elasticsearch_dsl_drf.filter_backends import (
+    FilteringFilterBackend,
+    IdsFilterBackend,
+    OrderingFilterBackend,
+    DefaultOrderingFilterBackend,
+    SearchFilterBackend,
+)
+from django_elasticsearch_dsl_drf.viewsets import BaseDocumentViewSet
+from django_elasticsearch_dsl_drf.pagination import PageNumberPagination
 
-# Create your views here.
+from .documents import MovieDocument
+from .serializers import MovieDocumentSerializer
+
+
+# class MovieDocumentView(BaseDocumentViewSet):
+#     document = MovieDocument
+#     serializer_class = MovieDocumentSerializer
+#     pagination_class = PageNumberPagination
+
+#     filter_backends = [
+#         FilteringFilterBackend,
+#         IdsFilterBackend,
+#         OrderingFilterBackend,
+#         DefaultOrderingFilterBackend,
+#         SearchFilterBackend,
+#     ]
+
+#     search_fields = ("title", "description", "year")
