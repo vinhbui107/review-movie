9f6506f00bf0fcc618ea128125fe7489c02a5f0c
vinhbui107 <vinhbui.tg@gmail.com>
2021-04-21 20:53:21 +0700

    update movie apis

--- api/apps/movies/views.py
+++ api/apps/movies/views.py
@@ -1,9 +1,16 @@
 from rest_framework.views import APIView
 from rest_framework.exceptions import status
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-from apps.movies.models import Movie, Genre, Rating
-from apps.accounts.models import User
+from apps.common.model_loaders import (
+    get_movie_model,
+    get_genre_model,
+    get_user_model,
+    get_comment_model,
+)
+from apps.common.responses import ApiMessageResponse
+from apps.movies.serializers import MovieSerializer
 
 
 class MovieItem(APIView):
@@ -11,8 +18,18 @@ class MovieItem(APIView):
     The API to get movie detail
     """
 
-    def get(self, request, slug):
-        return
+    def get(self, request, movie_id):
+        Movie = get_movie_model()
+        try:
+            movie = Movie.objects.get(pk=movie_id)
+            movie_serializer = MovieSerializer(
+                movie, context={"request": request}
+            )
+            return Response(movie_serializer.data, status=status.HTTP_200_OK)
+        except Movie.DoesNotExist:
+            return ApiMessageResponse(
+                "Movie not found", status=status.HTTP_404_NOT_FOUND
+            )
 
 
 class TrendingMovies(APIView):
@@ -21,32 +38,46 @@ class TrendingMovies(APIView):
     """
 
     def get(self, request):
-        return
+        Movie = get_movie_model()
+        movies = Movie.get_trending_movies()[:30]
+        movies_serializer = MovieSerializer(
+            movies, many=True, context={"request": request}
+        )
+        return Response(movies_serializer.data, status=status.HTTP_200_OK)
 
 
 class TopRatingMovies(APIView):
     """
-    The API to get top imdb rating movies
+    The API to get top rating movies
     """
 
     def get(self, request):
-        top_rating_movies = Movie.get_top_rating_movies()
-        return Response(top_rating_movies, status=status.HTTP_200_OK)
+        Movie = get_movie_model()
+        movies = Movie.get_top_rating_movies()[:30]
+        movies_serializer = MovieSerializer(
+            movies, many=True, context={"request": request}
+        )
+        return Response(movies_serializer.data, status=status.HTTP_200_OK)
 
 
-class HomeMovies(APIView):
+class RatingMovie(APIView):
     """
-    The API to get movie for home page
+    The API for rating of movie
     """
 
-    def get(self, request):
+    def get(self, request, movie_id):
+        return
+
+    def post(self, request, movie_id):
         return
 
 
-class MovieRatings(APIView):
+class UserRatingMovie(APIView):
     """
-    The API to get ratings of movie
+    The API to get rating of current user for movie
     """
 
-    def get(self, request):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, movie_id):
         return
