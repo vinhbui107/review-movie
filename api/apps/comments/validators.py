9e12bc250013d6674c4b76295fb6d6372ee394ea
vinhbui107 <vinhbui.tg@gmail.com>
2021-04-22 02:51:38 +0700

    update find object

--- api/apps/comments/validators.py
+++ api/apps/comments/validators.py
@@ -6,12 +6,12 @@ from apps.common.model_loaders import get_movie_model, get_comment_model
 def movie_id_exists(movie_id):
     Movie = get_movie_model()
 
-    if not Movie.objects.filter(pk=movie_id).exists():
-        raise ValidationError("The movie does not exist.")
+    if Movie.is_movie_not_exist(movie_id):
+        raise ValidationError("Movie does not exist.")
 
 
 def movie_comment_id_exists(movie_comment_id):
     Comment = get_comment_model()
 
-    if not Comment.objects.filter(pk=movie_comment_id).exist():
+    if Comment.is_comment_not_exist(comment_id=movie_comment_id):
         raise ValidationError("The comment does not exits.")
