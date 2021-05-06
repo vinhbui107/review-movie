c1a4a76b5b15b2946c4e9d3053237841d7180b3b
vinhbui107 <vinhbui.tg@gmail.com>
2021-05-06 23:10:08 +0700

    update admin site

--- api/apps/comments/admin.py
+++ api/apps/comments/admin.py
@@ -1,3 +1,11 @@
+from django import forms
 from django.contrib import admin
+from django.db import models
+from django.forms import CharField, Textarea, TextInput
 
-# Register your models here.
+from .models import Comment
+
+
+@admin.register(Comment)
+class CommentAdminConfig(admin.ModelAdmin):
+    model = Comment
--- api/apps/movies/admin.py
+++ api/apps/movies/admin.py
@@ -1,16 +1,41 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from django.forms import TextInput, Textarea, CharField
 from django import forms
+from django.contrib import admin
 from django.db import models
-from .models import Movie
+from django.forms import CharField, Textarea, TextInput
+
+from .models import Movie, Rating, Genre
 
 
 @admin.register(Movie)
 class MovieAdminConfig(admin.ModelAdmin):
     model = Movie
     search_fields = ("title",)
-    list_display = [
-        "id",
-        "title",
-    ]
+    list_filter = ("title", "year")
+    list_display = ["title", "year", "imdb_rating", "director", "slug"]
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "title",
+                    "year",
+                    "description",
+                    "imdb_rating",
+                    "rating_average",
+                    "rating_count",
+                    "poster",
+                    "slug",
+                )
+            },
+        ),
+    )
+
+
+@admin.register(Rating)
+class RatingAdminConfig(admin.ModelAdmin):
+    model = Rating
+
+
+@admin.register(Genre)
+class GenreAdminConfig(admin.ModelAdmin):
+    model = Genre
