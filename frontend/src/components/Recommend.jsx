f06206def1551464d7777e70bb9d1f3763496858
vinhbui107 <vinhbui.tg@gmail.com>
2021-05-11 23:18:16 +0700

    update link to movie

--- frontend/src/components/Recommend.jsx
+++ frontend/src/components/Recommend.jsx
@@ -4,23 +4,11 @@ import { Link, useHistory } from "react-router-dom";
 import "../style/components/Recommend.scss";
 
 function Recommend({ movies, title }) {
-    const history = useHistory();
-
-    const handleClick = (movieId) => {
-        history.push(`${movieId}`);
-        window.location.reload();
-    };
-
     const renderMovies = (movies) => {
-        return movies?.map((movie) => {
+        return movies?.map((movie, index) => {
             return (
-                <div className="recommendCard" key={movie.id}>
-                    <Link
-                        className="recommendCard__link"
-                        onClick={() => {
-                            handleClick(movie.id);
-                        }}
-                    >
+                <div className="recommendCard" key={index}>
+                    <Link className="recommendCard__link" to={`/movies/${movie.id}`}>
                         <Card.Img variant="top" src={movie.poster} loading="lazy" />
                         <p className="recommendCard__datetime mb-0">{movie.year}</p>
                         <div className="recommendCard__overlay">
