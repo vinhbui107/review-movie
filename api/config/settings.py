95f248f771fa7378a9bb974813518c89bbd2c53d
vinhbui107 <vinhbui.tg@gmail.com>
2021-05-14 03:09:14 +0700

    change lifetime jwt

--- api/config/settings.py
+++ api/config/settings.py
@@ -175,7 +175,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": True,
