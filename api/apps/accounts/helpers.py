2cf087e4f252c715e4d8cfe56ccdd1acdc0232cf
vinhbui107 <vinhbui.tg@gmail.com>
2021-05-27 22:50:41 +0700

    update path for store data image

--- api/apps/accounts/helpers.py
+++ api/apps/accounts/helpers.py
@@ -3,14 +3,14 @@ from os.path import splitext
 
 
 def upload_to_user_avatar_directory(user, filename):
-    return _upload_to_user_directory(user, filename=filename)
+    return _upload_to_user_directory(user=user, filename=filename)
 
 
 def _upload_to_user_directory(user, filename):
     extension = splitext(filename)[1].lower()
     new_filename = str(uuid.uuid4()) + extension
 
-    path = "users/%(user_uuid)s/" % {"user_uuid": str(user.uuid)}
+    path = "avatars/%(username)s/" % {"username": str(user.username)}
 
     return "%(path)s%(new_filename)s" % {
         "path": path,
