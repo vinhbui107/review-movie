c06f9c1fdda09dada13894c31643acd4324e8af6
vinhbui107 <vinhbui.tg@gmail.com>
2021-06-17 19:25:16 +0700

    Update component name

---
+++ frontend/src/components/ProfileChangePassword.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import { Form, Input, Select, Row, Col, Checkbox, Button, AutoComplete } from "antd";
+
+import "../style/components/ChangePasswordForm.scss";
+
+const formItemLayout = {
+    labelCol: {
+        span: 24,
+    },
+    wrapperCol: {
+        span: 24,
+    },
+};
+
+function ProfileChangePassword() {
+    const [form] = Form.useForm();
+
+    const onFinish = () => {};
+
+    return (
+        <div className="changePassword">
+            <Form {...formItemLayout} form={form} name="register" onFinish={onFinish} size="large" scrollToFirstError>
+                <Form.Item
+                    name="oldPassword"
+                    label="Old Password"
+                    rules={[
+                        {
+                            required: true,
+                            message: "Please input your password!",
+                        },
+                    ]}
+                    hasFeedback
+                >
+                    <Input.Password />
+                </Form.Item>
+
+                <Form.Item
+                    name="password"
+                    label="New Password"
+                    rules={[
+                        {
+                            required: true,
+                            message: "Please input your password!",
+                        },
+                    ]}
+                    hasFeedback
+                >
+                    <Input.Password />
+                </Form.Item>
+
+                <Form.Item
+                    name="confirm"
+                    label="Confirm New Password"
+                    dependencies={["password"]}
+                    hasFeedback
+                    rules={[
+                        {
+                            required: true,
+                            message: "Please confirm your password!",
+                        },
+                        ({ getFieldValue }) => ({
+                            validator(_, value) {
+                                if (!value || getFieldValue("password") === value) {
+                                    return Promise.resolve();
+                                }
+
+                                return Promise.reject(new Error("The two passwords that you entered do not match!"));
+                            },
+                        }),
+                    ]}
+                >
+                    <Input.Password />
+                </Form.Item>
+
+                <Form.Item className="btn-save">
+                    <Button type="primary" htmlType="submit">
+                        Save
+                    </Button>
+                </Form.Item>
+            </Form>
+        </div>
+    );
+}
+
+export default ProfileChangePassword;
