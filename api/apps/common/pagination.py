e9f64fdcf82e3b2d581256452c6cf9d983797421
vinhbui107 <vinhbui.tg@gmail.com>
2021-06-29 02:57:08 +0700

    rename panagtion file

--- api/apps/common/pagination.py
+++ api/apps/common/pagination.py
@@ -3,3 +3,8 @@ from rest_framework.pagination import PageNumberPagination
 
 class PageNumberPagination(PageNumberPagination):
     page_size_query_param = "page_size"
+
+
+class SmallResultsSetPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = "page_size"
