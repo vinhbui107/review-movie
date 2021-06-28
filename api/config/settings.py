b3b3fd5c2fea0151423eae8798e462cc8e1c184f
vinhbui107 <vinhbui.tg@gmail.com>
2021-06-29 02:58:31 +0700

    update page size

--- api/config/settings.py
+++ api/config/settings.py
@@ -179,7 +179,7 @@ REST_FRAMEWORK = {
         "rest_framework.parsers.JSONParser",
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 30,
+    "PAGE_SIZE": 20,
     "ORDERING_PARAM": "ordering",
 }
 
