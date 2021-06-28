5a324a935ebbf5f8e812e40376717699c9b36f65
vinhbui107 <vinhbui.tg@gmail.com>
2021-06-29 02:58:49 +0700

    update comment for validate func

--- api/apps/common/helpers.py
+++ api/apps/common/helpers.py
@@ -2,9 +2,11 @@ from rest_framework.serializers import Serializer
 
 
 def validate_data(schema_cls: Serializer, data: dict) -> dict:
-    """Validate data using Marshmallow schema
+    """
+    Validate data using Marshmallow schema
     Return validated data if success, raise ValidationError if failed
     """
+
     schema = schema_cls(data=data)
     schema.is_valid(raise_exception=True)
     return schema.validated_data
