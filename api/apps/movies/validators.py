9855e45a294698d1e4fff8549e11c3c0b7141249
vinhbui107 <vinhbui.tg@gmail.com>
2021-07-01 22:00:34 +0700

    add new validators

--- api/apps/movies/validators.py
+++ api/apps/movies/validators.py
@@ -1,17 +1,34 @@
-from rest_framework.exceptions import ValidationError
+from rest_framework.exceptions import ValidationError, NotFound
 
-from apps.common.model_loaders import get_movie_model, get_rating_model
+from apps.common.model_loaders import (
+    get_movie_model,
+    get_rating_model,
+    get_comment_model,
+)
 
 
-def movie_id_exists(movie_id):
+def movie_slug_exists(movie_slug):
     Movie = get_movie_model()
 
-    if Movie.is_movie_not_exist(movie_id):
-        raise ValidationError("Movie does not exist.")
+    if not Movie.objects.filter(slug=movie_slug).exists():
+        raise NotFound(
+            "The Movie does not exist.",
+        )
 
 
 def rating_id_exist(rating_id):
     Rating = get_rating_model()
 
-    if Rating.is_rating_not_exist(rating_id):
-        raise ValidationError("Rating does not exist")
+    if not Rating.objects.filter(pk=rating_id).exists():
+        raise NotFound(
+            "The Rating does not exist.",
+        )
+
+
+def movie_comment_id_exists(comment_id):
+    Comment = get_comment_model()
+
+    if not Comment.objects.filter(pk=comment_id).exists():
+        raise NotFound(
+            "The Comment does not exist.",
+        )
